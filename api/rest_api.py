9516598e8df5a48344872bdc3a71c50c50e1e6ea
griveralazo <g.riveralazo@hotmail.com>
2020-07-25 12:29:11 -0400

    Create rest_api.py

---
+++ api/rest_api.py
@@ -0,0 +1,2 @@
+## As this mini cv application must be able to communicate with other microservices, its fundamental
+## to have an api. Nevertheless, Its still no clear about the architecture of this "cv app".
